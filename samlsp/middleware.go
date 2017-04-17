d3169bc65836d036f717f38734a561f9c1d5b882
Daniel Eloff <dan.eloff@gmail.com>
2017-04-17 09:18:52 -0700

    Update middleware.go
    Use a more sensible past time that might work better on devices without a proper clock.

--- samlsp/middleware.go
+++ samlsp/middleware.go
@@ -240,7 +240,7 @@ func (m *Middleware) Authorize(w http.ResponseWriter, r *http.Request, assertion
 
 		// delete the cookie
 		stateCookie.Value = ""
-		stateCookie.Expires = time.Date(2016, 1, 1, 0, 0, 0, 0, time.UTC) // any time in the past, but not the zero time
+		stateCookie.Expires = time.Time{}.Add(time.Second) // past time as close to epoch as possible, but not zero time.Time{}
 		http.SetCookie(w, stateCookie)
 	}
 
