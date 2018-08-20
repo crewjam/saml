8ccd3e0090719ba7b919d6bc6792a31b6b097a84
Andrew Pilloud <apilloud@users.noreply.github.com>
2018-08-20 05:01:01 -0700

    idp: Allow intermediates to be encoded in signature (#127)

--- identity_provider.go
+++ identity_provider.go
@@ -91,6 +91,7 @@ type IdentityProvider struct {
 	Key                     crypto.PrivateKey
 	Logger                  logger.Interface
 	Certificate             *x509.Certificate
+	Intermediates           []*x509.Certificate
 	MetadataURL             url.URL
 	SSOURL                  url.URL
 	ServiceProviderProvider ServiceProviderProvider
@@ -702,6 +703,9 @@ func (req *IdpAuthnRequest) MakeAssertionEl() error {
 		PrivateKey:  req.IDP.Key,
 		Leaf:        req.IDP.Certificate,
 	}
+	for _, cert := range req.IDP.Intermediates {
+		keyPair.Certificate = append(keyPair.Certificate, cert.Raw)
+	}
 	keyStore := dsig.TLSCertKeyStore(keyPair)
 
 	signingContext := dsig.NewDefaultSigningContext(keyStore)
@@ -897,6 +901,9 @@ func (req *IdpAuthnRequest) MakeResponse() error {
 			Certificate: [][]byte{req.IDP.Certificate.Raw},
 			PrivateKey:  req.IDP.Key,
 			Leaf:        req.IDP.Certificate,
+		}
+		for _, cert := range req.IDP.Intermediates {
+			keyPair.Certificate = append(keyPair.Certificate, cert.Raw)
 		}
 		keyStore := dsig.TLSCertKeyStore(keyPair)
 
