b20013af9fe4ad9fc9a036d43b1ccd09e92c565a
Christopher Goulet <christophergoulet@outlook.com>
2019-10-29 07:22:53 -0700

    omit validUntil if empty (#190)

--- service_provider.go
+++ service_provider.go
@@ -141,7 +141,7 @@ func (sp *ServiceProvider) Metadata() *EntityDescriptor {
 								},
 							},
 						},
-						ValidUntil: validUntil,
+						ValidUntil: &validUntil,
 					},
 				},
 				AuthnRequestsSigned:  &authnRequestsSigned,
