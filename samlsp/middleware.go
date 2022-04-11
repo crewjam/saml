43777bd7c8206ce65700d4995cb7efa5c1bed936
Benjamin Neil <dev.benjamin.neil@gmail.com>
2022-04-11 15:49:33 -0600

    Respect redirectURI
    Ensures the redirectURI is respected even if RelayState was found in the acs parameters.

--- samlsp/middleware.go
+++ samlsp/middleware.go
@@ -183,7 +183,8 @@ func (m *Middleware) HandleStartAuthFlow(w http.ResponseWriter, r *http.Request)
 
 // CreateSessionFromAssertion is invoked by ServeHTTP when we have a new, valid SAML assertion.
 func (m *Middleware) CreateSessionFromAssertion(w http.ResponseWriter, r *http.Request, assertion *saml.Assertion, redirectURI string) {
-	if trackedRequestIndex := r.Form.Get("RelayState"); trackedRequestIndex != "" {
+	overridden := redirectURI != ""
+	if trackedRequestIndex := r.Form.Get("RelayState"); trackedRequestIndex != "" && !overridden {
 		trackedRequest, err := m.RequestTracker.GetTrackedRequest(r, trackedRequestIndex)
 		if err != nil {
 			m.OnError(w, r, err)
