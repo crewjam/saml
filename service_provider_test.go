19657d7c77d2dfe9ea1a812e7a8800029734831b
gbcards <jorge.mondalgo@jaak.ai>
2022-08-08 11:03:25 -0500

    Refactor: changed package crewjam to jaak-ai

--- service_provider_test.go
+++ service_provider_test.go
@@ -102,6 +102,7 @@ func TestSPCanSetAuthenticationNameIDFormat(t *testing.T) {
 	req, err = s.MakeAuthenticationRequest("", HTTPRedirectBinding, HTTPPostBinding)
 	assert.Check(t, err)
 	assert.Check(t, is.Equal(string(EmailAddressNameIDFormat), *req.NameIDPolicy.Format))
+
 }
 
 func TestSPCanProduceMetadataWithEncryptionCert(t *testing.T) {
