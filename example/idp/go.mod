22de5ecbb4fb07f8878e5408366278680f3e6f96
Adrian Castillo <adrian.castillo@hidglobal.com>
2025-05-04 15:32:09 +0200

    Make idp a complete application for deployment

---
+++ example/idp/go.mod
@@ -0,0 +1,20 @@
+module github.com/adricasti/samlidp
+
+go 1.23
+
+toolchain go1.24.2
+
+require (
+	github.com/crewjam/saml v0.5.1
+	golang.org/x/crypto v0.33.0
+)
+
+require (
+	github.com/beevik/etree v1.5.0 // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/mattermost/xml-roundtrip-validator v0.1.0 // indirect
+	github.com/russellhaering/goxmldsig v1.4.0 // indirect
+)
+
+// Replace the remote saml module with your local version
+replace github.com/crewjam/saml => ../../
--- example/service.go
+++ example/service.go
@@ -87,45 +87,14 @@ func ServeWhoami(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var (
-	key = []byte(`-----BEGIN RSA PRIVATE KEY-----
-MIICXgIBAAKBgQDU8wdiaFmPfTyRYuFlVPi866WrH/2JubkHzp89bBQopDaLXYxi
-3PTu3O6Q/KaKxMOFBqrInwqpv/omOGZ4ycQ51O9I+Yc7ybVlW94lTo2gpGf+Y/8E
-PsVbnZaFutRctJ4dVIp9aQ2TpLiGT0xX1OzBO/JEgq9GzDRf+B+eqSuglwIDAQAB
-AoGBAMuy1eN6cgFiCOgBsB3gVDdTKpww87Qk5ivjqEt28SmXO13A1KNVPS6oQ8SJ
-CT5Azc6X/BIAoJCURVL+LHdqebogKljhH/3yIel1kH19vr4E2kTM/tYH+qj8afUS
-JEmArUzsmmK8ccuNqBcllqdwCZjxL4CHDUmyRudFcHVX9oyhAkEA/OV1OkjM3CLU
-N3sqELdMmHq5QZCUihBmk3/N5OvGdqAFGBlEeewlepEVxkh7JnaNXAXrKHRVu/f/
-fbCQxH+qrwJBANeQERF97b9Sibp9xgolb749UWNlAdqmEpmlvmS202TdcaaT1msU
-4rRLiQN3X9O9mq4LZMSVethrQAdX1whawpkCQQDk1yGf7xZpMJ8F4U5sN+F4rLyM
-Rq8Sy8p2OBTwzCUXXK+fYeXjybsUUMr6VMYTRP2fQr/LKJIX+E5ZxvcIyFmDAkEA
-yfjNVUNVaIbQTzEbRlRvT6MqR+PTCefC072NF9aJWR93JimspGZMR7viY6IM4lrr
-vBkm0F5yXKaYtoiiDMzlOQJADqmEwXl0D72ZG/2KDg8b4QZEmC9i5gidpQwJXUc6
-hU+IVQoLxRq0fBib/36K9tcrrO5Ba4iEvDcNY+D8yGbUtA==
------END RSA PRIVATE KEY-----
-`)
-	cert = []byte(`-----BEGIN CERTIFICATE-----
-MIIB7zCCAVgCCQDFzbKIp7b3MTANBgkqhkiG9w0BAQUFADA8MQswCQYDVQQGEwJV
-UzELMAkGA1UECAwCR0ExDDAKBgNVBAoMA2ZvbzESMBAGA1UEAwwJbG9jYWxob3N0
-MB4XDTEzMTAwMjAwMDg1MVoXDTE0MTAwMjAwMDg1MVowPDELMAkGA1UEBhMCVVMx
-CzAJBgNVBAgMAkdBMQwwCgYDVQQKDANmb28xEjAQBgNVBAMMCWxvY2FsaG9zdDCB
-nzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEA1PMHYmhZj308kWLhZVT4vOulqx/9
-ibm5B86fPWwUKKQ2i12MYtz07tzukPymisTDhQaqyJ8Kqb/6JjhmeMnEOdTvSPmH
-O8m1ZVveJU6NoKRn/mP/BD7FW52WhbrUXLSeHVSKfWkNk6S4hk9MV9TswTvyRIKv
-Rsw0X/gfnqkroJcCAwEAATANBgkqhkiG9w0BAQUFAAOBgQCMMlIO+GNcGekevKgk
-akpMdAqJfs24maGb90DvTLbRZRD7Xvn1MnVBBS9hzlXiFLYOInXACMW5gcoRFfeT
-QLSouMM8o57h0uKjfTmuoWHLQLi6hnF+cvCsEFiJZ4AbF+DgmO6TarJ8O05t8zvn
-OwJlNCASPZRH/JmF8tX0hoHuAQ==
------END CERTIFICATE-----
-`)
-)
-
 func main() {
 	rootURLstr := flag.String("url", "http://localhost:8090", "The base URL of this service")
 	idpMetadataURLstr := flag.String("idp", "http://localhost:8080/metadata", "The metadata URL for the IDP")
+	certFile := flag.String("cert", "service.cert", "The certificate file path")
+	keyFile := flag.String("key", "service.key", "The private key file path")
 	flag.Parse()
 
-	keyPair, err := tls.X509KeyPair(cert, key)
+	keyPair, err := tls.LoadX509KeyPair(*certFile, *keyFile)
 	if err != nil {
 		panic(err) // TODO handle error
 	}
